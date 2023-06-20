3497fc9a770bba8e23c6fd1a59e024fd8fdd4210
Giordano Temple <giordanotemple@gmail.com>
2023-06-20 09:44:01 -0400

    lesson 1.1

---
+++ cs-61b-datastructures/1.1/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
+}
+
+
+/**
+ * all code must be part of a class
+ * classes are defined with 'public class CLASSNAME'
+ */
