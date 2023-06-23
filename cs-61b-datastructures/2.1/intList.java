fe256d2fc88e7f7c1d8a9044d7e25d2cc57e74a6
Giordano Temple <giordanotemple@gmail.com>
2023-06-23 11:38:23 -0400

    recursion!

--- cs-61b-datastructures/2.1/intList.java
+++ cs-61b-datastructures/2.1/intList.java
@@ -1,21 +1,42 @@
-public class intList {
+public class IntList {
   public int first;
   public IntList rest;
 
-  public IntList(int f, IntList r) {
+  public void IntList(int f, IntList r) {
     first = f;
     rest = r;
   }
+  // recursion 
+  public int size() {
+    if (rest == null) {
+      return 1;
+    }
+    return 1 + this.rest.size();
+  }
+
+  public int iterativeSize() {
+    IntList p = this;
+    int totalSize = 0;
+    while (p != null) {
+      totalSize += 1;
+      p = p.rest;
+    }
+    return totalSize;
+  }
 
   public static void main(String[] args) {
-    IntList L = new intList();
+    IntList L = new IntList();
     L.first = 5;
     L.rest = null;
 
     L.rest = new IntList();
     L.rest.first = 10;
 
-    L.rest.rest = newIntList();
+    L.rest.rest = new IntList();
     L.rest.rest.first = 15;
+
+    System.out.println(L.size());
+    System.out.println(L.iterativeSize());
+
   }
 }
